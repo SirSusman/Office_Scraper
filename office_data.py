1b33dcc470190e338cac93b55db876e1d613f21b
motazmo <brandonsusini@gmail.com>
2021-09-23 15:52:44 -0400

    Consolidated seperateMultipleLines method to seperate names and add to dict

--- office_data.py
+++ office_data.py
@@ -4,39 +4,40 @@
 import requests, bs4, re, pickle
 mainSite = requests.get("https://transcripts.foreverdreaming.org/viewforum.php?f=574")
 epIndex = 25301
-#for i in range(0,197):
-#while epIndex <= 25498:
 betweenP = re.compile(r"/a*<\s*p[^>]*>(.*?)<\s*/\s*p>/g")
 betweenStrong = re.compile(r"/<\s*strong[^>]*>(.*?)<\s*/\s*strong>/g")
-#Episode stuuuuuuff
-episodeData = {}
-characterLines = {}
-#Save to files incase something happens to the forums
-#{sceneNum, sceneData}
-#sceneData = lineData --> {lineIndex: [name,line]}
-
-
-#Remove scene format, add all lines as seperate lines into an array, maybe
-#parse speaker later.
 
 scenes = {}
 character_lines = {}
-debug_statements = False
+debug_statements = True
 testing = True
 def seperateMultipleNames(line):
+    split = []
     if "&" in line:
         split = line.split("&")
     elif "and" in line:
         split = line.split("and")
-
-    for count,char in enumerate(split):
-        split[count] = char.strip()
+    if len(split)>0:
+        for count,char in enumerate(split):
+            split[count] = char.strip()
     #print(first_name + ","+ second_name)
-    return split
-
-def addNamesToDict():
+    splitLine = line.split(":")[-1].strip()
+    for name in split:
+        if name in character_lines.keys():
+            character_lines[name].append(splitLine)
+        else:
+            character_lines[name] = []
+    
+    #return split
     return
-def downloadScript():
+
+def addNamesToDict(names):
+    for name in names:
+        
+    return
+
+
+def downloadScript(epIndex):
     while epIndex <= 25498:
         try:
             current_episode = requests.get("https://transcripts.foreverdreaming.org/viewtopic.php?f=574&t=" + str(epIndex))
@@ -46,6 +47,10 @@ def downloadScript():
             strippedText = epText[0].text.strip()
             strippedLines = strippedText.splitlines()
             i = 1
+
+            #NOT GRABBING LINE FROM MULTIPLE NAMES
+
+
             for line in strippedLines:
                 try:
                     if len(line) > 1:
@@ -65,11 +70,9 @@ def downloadScript():
                         #      splitLine)
                         #lineData =[name,splitLine]
                         #scene.append(lineData)
-                        for name in names:
-                            if name in character_lines.keys():
-                                character_lines[name].append(splitLine)
-                            else:
-                                character_lines[name] = []
+                        #if(len(names)>0):
+                        #    addNamesToDict(names)
+                        
                         i+=1
                         #characterLines[tempList[0]] = tempList[1]
                     else:
@@ -84,10 +87,9 @@ def downloadScript():
         except Exception as e:
             print(f"Error Getting transcript{epIndex}:::{e}")
 
-    
-def printLines(d):
-    for k,v in d.items():
-        print(k,v)
+def printKeys():
+    for key in character_lines.keys():
+        print(key)
 
         
 def printScenes():
@@ -101,9 +103,11 @@ def printScenes():
             #print("--Name:", scenes[s][l][0],"\n----Line:",scenes[s][l][1])
         i+= 1
 
-        
+
+downloadScript(epIndex)
+
 if(debug_statements):
-    printLines()
+    printKeys()
     print(epIndex)
 if not testing:
     character_lines = pickle.load(open("/Users/brandon/Documents/Code/Python/TypingBot/character_dict.p","rb"))
